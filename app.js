4f698cf32cdcee9e52fe4abec563d1c685281f30
Muzammil803 <syedmuzammil691@gmail.com>
2022-07-12 23:31:42 +0500

    done

---
+++ app.js
@@ -0,0 +1,53 @@
+
+
+// ============================
+
+// ============================
+
+// var number = 3.45214;
+// document.write("<h1> Number : " + number + "</h1>");
+// document.write("<h1> Round Off Value : " + Math.round(number) + "</h1>");
+// document.write("<h1> Floor value :  " + Math.floor(number) + "</h1>");
+// document.write("<h1> Ceil value : " + Math.ceil(number) + "</h1>" )
+// var num = -2.673;
+// document.write("<h1> Number : " + num + "</h1>");
+// document.write("<h1> Number : " + Math.round(num) + "</h1>");
+// document.write("<h1> Number : " + Math.floor(num) + "</h1>");
+// document.write("<h1> Number : " + Math.ceil(num) + "</h1>");
+// var num = prompt("Enter the value");
+// var absoluteValue = Math.abs(num);
+// document.write("<h1> Absolute value of " + num + " is " + absoluteValue + "</h1>");
+// var randomNumber = Math.random() 
+// ==============================================
+
+// =================================================
+
+// var randomNumber = Math.random();
+// var num = Math.round(randomNumber*6);
+// document.write("<h1> Random Dice Value is: " + num + "</h1>");
+// var randomNumber = Math.random();
+// var num = Math.round(randomNumber*5);
+// document.write("<h1> Random Dice Value is: " + num + "</h1>");
+// var num = Math.random();
+// var diceNum = Math.round(num * 2);
+// if(diceNum === 2){
+//     document.write("<h1> Random Dice value is: Heads </h1>");
+// }else if(diceNum === 1){
+//     document.write("<h1> Random Dice value is: Tails");
+// }
+// =============================================
+// =================================================
+
+// var num = Math.random();
+// document.write("<h1> Random value between 1 & 100 : " + Math.round(num * 99) + "</h1>");
+// var weight = prompt("Enter your weight");
+// document.write("<h1> The weight of user is: " + parseFloat(weight) + " Kg </h1>");
+// var num = Math.random();
+// var userNum = prompt("Enter number between 1 & 10");
+// var secretNum = Math.round(num * 9); 
+// if(secretNum === Number(userNum)){
+//     document.write("<h1>  Congratulation </h1>");
+// }else{
+//     document.write("<h1> Please try again");
+// }
+// console.log(secretNum) 
